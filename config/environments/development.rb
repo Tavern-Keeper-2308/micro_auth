31f3df941507b46b0d0adcd2f1d2def286396f75
Xander Hendry <vhendry.coding@gmail.com>
2024-02-21 15:36:41 -0500

    change localhost port to 4000

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
   # Added as part of base Devise setup
 
   # In the development environment your application's code is reloaded any time
